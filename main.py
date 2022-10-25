a58a1920a63322336f86584932002590ee9d6f37
Victor Matias <victormatiasilva@gmail.com>
2022-10-24 23:23:49 -0300

    Segmentacao

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import os
+
 from mlforkids import MLforKidsImageProject
 from use_salved_model import UseSalvedModel
+from segmentacao import executar_segmentacao
+
 # treat this key like a password and keep it secret!
 key = "0b9bc1f0-4446-11ed-9c85-e9175b095422a46f8bf5-ac45-4c45-8d5e-344a7aa7139e"
 
 # this will train your model and might take a little while
 load_or_train = input('Você deseja treinar um novo modelo ou carregar o modelo salvo?(treinar/carregar): ')
+
 if load_or_train.lower() == 'treinar':
     myproject = MLforKidsImageProject(key)
     myproject.train_model()
@@ -17,18 +22,34 @@ while arquivo != 'sair':
 
     try:
         #Escolhendo o arquivo para testar
-        arquivo = str(input("Digite o nome do arquivo que está dentro da pasta de testes sem a extensão: "))
-
-        # CHANGE THIS to the image file you want to recognize
-        imagem = myproject.prediction("./imagens_testes/" + arquivo + ".jpg")
+        arquivo = str(input("Digite o nome do arquivo que está dentro da pasta de testes: "))
 
     except FileNotFoundError:
         print("Arquivo não existente.")
 
     else:
-        
-        label = imagem["class_name"]
-        confidence = imagem["confidence"]
 
-        # CHANGE THIS to do something different with the result
-        print ("result: '%s' with %d%% confidence" % (label, confidence))
+        executar_segmentacao("./imagens_testes/" + arquivo)
+
+        # Caminho
+        path = os.getcwd()
+        caminho = path + r"\.temp_images"
+
+        filelist = [f for f in os.listdir(caminho)]
+        
+        item_lista = []
+
+        for file in filelist:
+
+            # CHANGE THIS to the image file you want to recognize
+            imagem = myproject.prediction("./.temp_images/" + file)
+            
+            label = imagem["class_name"]
+            confidence = imagem["confidence"]
+
+            item_lista.append((label,str(int(confidence))+"%"))
+
+            # CHANGE THIS to do something different with the result
+            #print ("result: '%s' with %d%% confidence" % (label, confidence))
+
+        print(item_lista)
---
+++ segmentacao.py
@@ -0,0 +1,69 @@
+ # Bibliotecas
+import cv2
+import os
+import numpy as np
+from imutils import contours
+
+def verificar_pasta(caminho):
+    # Verificar se pasta Repositorio existe
+    if os.path.isdir(caminho) == False:
+        os.mkdir(caminho)
+
+def deletar_arquivos(caminho):
+    filelist = [f for f in os.listdir(caminho)]
+    for f in filelist:
+        os.remove(os.path.join(caminho , f))
+
+def executar_segmentacao(imagem):
+
+    # Caminho
+    path = os.getcwd()
+    caminho = path + r"\.temp_images"
+
+    verificar_pasta(caminho)
+    deletar_arquivos(caminho)
+
+    # Imagem com os dados
+    img = cv2.imread(path + r"\\" + imagem)
+
+    # Pre-Processamento
+    # Imagem em tons de cinza
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Filtro de suavizacao
+    img_blur = cv2.medianBlur(gray, 3)
+
+    # Detectcao de bordas
+    edges = cv2.Canny(img_blur, 50, 255)
+
+    # Morfologia matematica
+    kernel = np.ones((9,9), np.uint8)
+    img_dilation = (cv2.dilate(edges, kernel, iterations=1))
+
+    #Imagem final
+    img_final = img_blur
+
+    # Segementacao
+    ret,thresh1 = cv2.threshold(img_final,127,255,cv2.THRESH_BINARY)
+
+    # Localizar contornos
+    # cv2.RETR_TREE -> recupera todos os contornos e reconstrói uma hierarquia completa de contornos aninhados.
+    # cv2.CHAIN_APPROX_SIMPLE -> Armazena apenas os pontos iniciais e finais dos contornos detectados.
+    cnts = cv2.findContours(img_dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    # Ordenacao dos contornos encontrados
+    (cnts, _) = contours.sort_contours(cnts, method='left-to-right')
+
+    ROI_number = 0
+    for c in cnts:
+        # Calcular a area encontrada nos contornos. Se area maior que 1100 provavelmente e um numero
+        area = cv2.contourArea(c)
+        if area > 1100 :
+            x,y,w,h = cv2.boundingRect(c)  # Coordenadas dos contornos      
+            #cv2.rectangle(img, (x - 1, y - 1), (x + 1 + w, y + 1 + h), (0, 0, 255), 1) # Selecionar um retangulo
+            ROI = thresh1[y-5:y+h+5, x-10:x+w+10]
+            ROI = cv2.resize(ROI, (1000,1000), interpolation = cv2.INTER_AREA) # Salvar a imagem 28x28 (Modelo treinado MNIST)
+            #cv2.drawContours(img, [c], -1, (0,255,0), -1) # Desenhar o contorno localizado
+            #ROI = cv2.bitwise_not(ROI)
+            cv2.imwrite(path + f'\\.temp_images\\img_{ROI_number}.jpg', ROI)
+            ROI_number += 1
